6103cf9583594d856b3d9dc0f0be1ff8c2ceea90
liuwan950628 <1027968784@qq.com>
2018-11-01 22:22:51 +0800

    again

---
+++ src/page/home/components/header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont backicon">&#xe624;</div>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~/~/src/assets/styles/reset.css';
+    .header
+      display flex;
+      line-height: .86rem;
+      background $bgColor
+      color #ffffff
+      .header-left
+        width: .64rem
+        float: left
+        .backicon
+            text-align center
+            font-size .4rem
+      .header-input
+        flex: 1
+        background #ffffff
+        border-radius: .1rem
+        line-height .64rem
+        height: .64rem
+        margin-top: .12rem
+        margin-left: .2rem
+        padding-left .2rem
+        color: #ccc
+      .header-right
+        min-width  1.04rem
+        padding 0.1rem
+        float right
+        text-align center
+        color #ffffff
+        .arrow-icon
+            margin-left -.04rem
+            font-size .24rem
+</style>
---
+++ src/page/home/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+        <Header></Header>
+    </div>
+</template>
+<script>
+import Header from './components/header'
+export default{
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
+<style lang="" scoped>
+</style>
